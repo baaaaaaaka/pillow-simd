c5cded31e78ad81bbdf75c4679962cdf9bcf1937
jawei <jawei@nvidia.com>
2026-01-30 16:33:43 +0800

    Install numpy on macOS CI.
    Ensure numpy is available on macOS (including 3.12-dev) for bmp partial load tests.

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -13,8 +13,8 @@ python3 -m pip install -U pytest-cov
 python3 -m pip install -U pytest-timeout
 python3 -m pip install pyroma
 
-# TODO Remove condition when NumPy supports 3.12
-if ! [ "$GHA_PYTHON_VERSION" == "3.12-dev" ]; then python3 -m pip install numpy ; fi
+# NumPy is required for bmp partial load tests
+python3 -m pip install numpy
 
 # extra test images
 pushd depends && ./install_extra_test_images.sh && popd

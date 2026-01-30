3603356cf7c39e7650d4868ea5511f65f1cf783d
jawei <jawei@nvidia.com>
2026-01-30 14:14:05 +0800

    Stabilize Ubuntu Qt tests and Windows artifacts.
    Force Qt to use an offscreen backend on Linux CI and make Windows artifact names unique per matrix job.

--- .ci/test.sh
+++ .ci/test.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [[ "$(uname)" == "Linux" ]]; then
+    export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-offscreen}
+fi
+
 python3 -c "from PIL import Image"
 
 python3 -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term Tests $REVERSE
